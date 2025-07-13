5a5b9b30e2d4ddb84fc9c145cf2f442534b93229
nishan-singh <singh007nishan@gmail.com>
2025-07-13 12:13:41 +0200

    commment out plugins which are not needed anymore

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,9 +1,9 @@
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,46 +16,51 @@ vim.opt.rtp:prepend(lazypath)
 --
 local plugins = {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
-
+  { "tpope/vim-sleuth" }, -- Detect tabstop and shiftwidth automatically
+  {
+    "barrett-ruth/live-server.nvim",
+    build = "pnpm add -g live-server",
+    cmd = { "LiveServerStart", "LiveServerStop" },
+    config = true,
+  },
   --  This is equivalent to:
   --    require('telescope').setup({})
-  require 'custom.plugins.telescope',
-  require 'custom.plugins.which-key',
-  require 'custom.plugins.lspconfig',
-  require 'custom.plugins.conform',
-  require 'custom.plugins.treesitter',
-  require 'custom.plugins.nvim-cmd',
-  require 'custom.plugins.tokyonight',
-  require 'custom.plugins.oil',
-  require 'custom.plugins.comments',
-  require 'custom.plugins.mini',
-  require 'custom.plugins.vim-fugitive',
-  require 'custom.plugins.hard-time',
-  require 'custom.plugins.copilot',
+  require "custom.plugins.telescope",
+  require "custom.plugins.which-key",
+  require "custom.plugins.lspconfig",
+  require "custom.plugins.conform",
+  require "custom.plugins.treesitter",
+  -- require "custom.plugins.nvim-cmd",
+  require "custom.plugins.tokyonight",
+  require "custom.plugins.oil",
+  require "custom.plugins.comments",
+  require "custom.plugins.mini",
+  require "custom.plugins.vim-fugitive",
+  -- require "custom.plugins.hard-time",
+  require "custom.plugins.copilot",
   --
-  require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  -- require "kickstart.plugins.indent_line",
+  -- require "kickstart.plugins.lint",
+  require "kickstart.plugins.autopairs",
+  -- require "kickstart.plugins.gitsigns", -- adds gitsigns recommend keymaps
 }
 
 local opts = {
   ui = {
     icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      require = "🌙",
+      source = "📄",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
     },
   },
 }
@@ -71,4 +76,4 @@ local opts = {
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup(plugins, opts)
+require("lazy").setup(plugins, opts)
