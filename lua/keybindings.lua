2ed418880dc3a0f6c838b73f2588bb5991f05d4e
nishan-singh <singh007nishan@gmail.com>
2025-09-16 10:05:31 +0200

    add keybings for finding dirs with fzf-lua and fd

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -30,3 +30,25 @@ vim.keymap.set("n", "<leader>cl", "<cmd>checkhealth lsp<CR>")
 
 -- Disable Backspace in insert + command-line modes
 vim.keymap.set({ "i", "c" }, "<BS>", "<Nop>", { noremap = true, silent = true })
+
+-- Set <C-a> to move to beginning of line and set <C-b> to one character back
+vim.keymap.set('c', '<C-a>', '<Home>', { noremap = true })
+vim.keymap.set('c', '<C-b>', '<Left>', { noremap = true })
+
+-- <leader>od -> pick a directory with fzf-lua, open it in oil
+vim.keymap.set("n", "<leader>od", function()
+  require("fzf-lua").fzf_exec("fd -td", { -- list only dirs (requires fd)
+    prompt = "Dirs> ",
+    actions = {
+      -- run when you hit <CR>
+      ["default"] = function(selected)
+        local dir = selected and selected[1]
+        if not dir or dir == "" then return end
+        -- make absolute (netrw/oil like absolute; optional)
+        dir = vim.fn.fnamemodify(dir, ":p")
+        require("oil").open(dir)
+      end,
+    },
+  })
+end)
+
