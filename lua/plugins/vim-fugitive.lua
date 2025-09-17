9f1bfa4452f14088731f7190faed373c3f6d3e6d
nishan-singh <singh007nishan@gmail.com>
2025-09-17 16:11:35 +0200

    remove optional dependencies from fugitive

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -2,10 +2,5 @@ return {
   {
     'tpope/vim-fugitive',
     event = 'VeryLazy',
-    dependencies = {
-      'tpope/vim-rhubarb',
-      'tpope/vim-obsession',
-      'tpope/vim-unimpaired',
-    },
   },
 }
